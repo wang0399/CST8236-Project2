f3803faecb3d60f0f75e77df5b6b0825bc4bb51a
wang0399 <wang0399@algonquinlive.com>
2016-11-16 18:43:15 -0500

    Project 2 Asset and ProjectSettings
    Graphics Project 2 Asset and projectSettings

---
+++ Assets/ChangePrrojection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePrrojection : MonoBehaviour {
+
+    new Camera camera;
+    bool projectSwitch;
+
+    // Use this for initialization
+    void Start () {
+        camera = GetComponent<Camera>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            projectSwitch = !projectSwitch; //switch boolean value
+        //change projection 
+        camera.orthographic = projectSwitch;
+    }
+}
---
+++ Assets/SwitchMaterial.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchMaterial : MonoBehaviour {
+
+    public Material[] materialsToChnage;
+    public int rotationSpeed;
+    public MeshRenderer meshRenderer;
+
+    // Use this for initialization
+    void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // get key presses and change material
+    void Update() {
+        //rotate shape along x and y axis 
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+
+        //Evaluate keypress and change materia
+        try {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                if (materialsToChnage.Length >= 1) {
+                    meshRenderer.material = materialsToChnage[0];
+                }
+            }         
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+                if (materialsToChnage.Length >= 2)
+                {
+                    meshRenderer.material = materialsToChnage[1];
+                }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+                if (materialsToChnage.Length >= 3)
+                {
+                    meshRenderer.material = materialsToChnage[2];
+                }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+                if (materialsToChnage.Length >= 4)
+                {
+                    meshRenderer.material = materialsToChnage[3];
+                }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+                if (materialsToChnage.Length >= 5)
+                {
+                    meshRenderer.material = materialsToChnage[4];
+                }
+
+       if (Input.GetKeyDown(KeyCode.Alpha6))
+                if (materialsToChnage.Length >= 6)
+                {
+                    meshRenderer.material = materialsToChnage[5];
+                }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("Exception occured in SwitchMaterial.cs - update() " + ex.Message); //test 
+        }
+
+      }
+}
+
